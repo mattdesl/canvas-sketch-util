d405348a9bdb92f301fbc912179ab3294cfc264e
mattdesl <dave.des@gmail.com>
2018-08-15 13:55:43 -0400

    testing a sketch

---
+++ test/sketch-1.js
@@ -0,0 +1,14 @@
+const canvasSketch = require('canvas-sketch');
+
+const settings = {
+  dimensions: [ 2048, 2048 ]
+};
+
+const sketch = () => {
+  return ({ context, width, height }) => {
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, width, height);
+  };
+};
+
+canvasSketch(sketch, settings);
