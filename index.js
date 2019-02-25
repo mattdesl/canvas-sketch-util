e9521120083259154d6a2d34a64ced3f49cb1bd6
mattdesl <dave.des@gmail.com>
2019-02-25 21:01:45 +0000

    fix index

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 module.exports = {
   random: require('./random'),
-  math: require('./math')
+  math: require('./math'),
+  penplot: require('./penplot.js'),
+  geometry: require('./geometry.js'),
+  shader: require('./shader.js')
 };
